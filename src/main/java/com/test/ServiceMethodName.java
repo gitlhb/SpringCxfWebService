c903571f1205e39c672e236cb22cd151a7edb972
gitliuhongbing <liuhb@guahao.com>
2018-08-17 16:00:13 +0800

    增加一个记录日志的方法

--- src/main/java/com/test/ServiceMethodName.java
+++ src/main/java/com/test/ServiceMethodName.java
@@ -4,6 +4,41 @@ package com.test;
  * Created by liuhongbing on 2018/8/15.
  */
 public enum ServiceMethodName {
+    /**
+     * 报告方法 提供给平台服务调用
+     */
+    CheckService,
+    /**
+     * 用户方法 提供给平台服务调用
+     */
+    UserService,
+    /**
+     * 日志方法 提供给平台服务调用
+     */
+    CustomService,
+    /**
+     * 住院方法 提供给平台服务调用
+     */
+    InHospitalFeeService,
+    /**
+     * 排队方法 提供给平台服务调用
+     */
+    LineService,
+    /**
+     * 缴费方法 提供给平台服务调用
+     */
+    PayService,
+    /**
+     * 公共方法 提供给平台服务调用
+     */
+    PubService,
+    /**
+     * 门诊病历方法 提供给平台服务调用
+     */
+    RecordService,
+    /**
+     * 挂号方法 提供给平台服务调用
+     */
+    RegService,
 
-    UserService
 }
